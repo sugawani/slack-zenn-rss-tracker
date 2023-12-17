a2325eab7f182f773e3c4cc01e5fcefda9a78866
sugawani <nekorollykk@gmail.com>
2023-12-17 23:59:43 +0900

    publicationName 単位でユーザ一覧取得

--- src/index.ts
+++ src/index.ts
@@ -21,20 +21,62 @@ type RSSData = {
 			generator: string;
 			lastBuildDate: string;
 			"atom:link": string;
-			item: {
-				title: string;
-				link: string;
-				description: string;
-				pubDate: string;
-				guid: string;
-				"dc:creator": string;
-			}[];
+			item: RSSItem[];
 		}
 	}
 }
 
+type RSSItem = {
+	title: string;
+	link: string;
+	description: string;
+	pubDate: string;
+	guid: string;
+	"dc:creator": string;
+}
+
 type KVKey = string;
-type KVValue = RSSData["rss"]["channel"]["item"][number]["guid"]
+type KVValue = RSSItem["guid"];
+
+type Articles = {
+	articles: Article[];
+}
+
+type Article = {
+  id: number;
+  post_type: "Article";
+  title: string;
+  slug: string;
+  published: boolean;
+  comments_count: number;
+  liked_count: number;
+  body_letters_count: number;
+  article_type: "tech" | "idea";
+  emoji: string;
+  is_suspending_private: boolean;
+  published_at: string;
+  body_updated_at: string;
+  source_repo_updated_at: string;
+  path: string;
+  user: User;
+  publication: Publication | null;
+};
+
+type Publication = {
+  id: number;
+  name: string;
+  avatar_small_url: string;
+  display_name: string;
+  beta_stats: boolean;
+  avatar_registered: boolean;
+};
+
+type User = {
+  id: number;
+  username: string;
+  name: string;
+  avatar_small_url: string;
+};
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
@@ -46,7 +88,9 @@ export default {
 			return `Add User: ${userID} Successfully`
 		})
 		app.command("/add-watch-publication", async ( { context, payload }) => {
-			console.log(`/payload posted to add-publication: ${payload.text}`)
+			const publicationName = payload.text
+			const publicationUserIDs = await getPublicationUserIDs(publicationName)
+			console.log(`publicationUserIDs: ${publicationUserIDs}`)
 			return "hello from /add-watch-publication"
 		})
 		return await app.run(request, ctx)
@@ -78,4 +122,17 @@ const getArticles = async (userID: KVKey, env: Env): Promise<KVValue[]> => {
 		return [];
 	}
 	return JSON.parse(articles) as KVValue[];
+}
+
+const getPublicationUserIDs = async(publicationName: string): Promise<string[]> => {
+	const res = await fetch(`https://zenn.dev/api/articles?publication_name=${publicationName}`)
+	if (res.status === 404) {
+		throw new Error(`publication ${publicationName} not found`);
+	}
+	const data = await res.json() as Articles;
+	if (data.articles.length === 0) {
+		throw new Error(`publication ${publicationName} has no articles`);
+	}
+	const userNames = data.articles.map((article) => article.user.username);
+	return [...new Set(userNames)]
 }
