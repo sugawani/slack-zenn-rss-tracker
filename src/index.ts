ff1eed21c219254e547ce0fa06d8a02c8b401fbb
sugawani <nekorollykk@gmail.com>
2023-12-17 22:51:37 +0900

    XML取ってきてパースする処理実装

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
 import { SlackApp, SlackEdgeAppEnv } from "slack-cloudflare-workers";
+import { XMLParser } from "fast-xml-parser";
 
 export interface Env extends SlackEdgeAppEnv {
-	KV: KVNamespace;
+	zenn_articles: KVNamespace;
 	POST_CHANNEL_ID: string;
+}
+
+type RSSData = {
+	channel: {
+		title: string;
+		link: string;
+		description: string;
+		image: {
+			url: string;
+			title: string;
+			link: string;
+		}
+		language: string;
+		generator: string;
+		lastBuildDate: string;
+		"atom:link": string;
+		item: {
+			title: string;
+			link: string;
+			description: string;
+			pubDate: string;
+			guid: string;
+			"dc:creator": string;
+		}[];
+	}
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const app = new SlackApp({ env })
 		app.command("/add-watch-user", async ( { context, payload }) => {
-			console.log(`payload posted to add-watch-user ${payload.text}`)
+			const data = await fetchUserRSS(payload.text);
+			console.log('%o', data)
 			return "hello from /add-watch-user"
 		})
 		app.command("/add-watch-publication", async ( { context, payload }) => {
@@ -23,3 +50,13 @@ export default {
 		console.log(`trigger fired at ${event.cron}`);
 	},
 };
+
+const fetchUserRSS = async (userID: string): Promise<RSSData> => {
+	const res = await fetch(`https://zenn.dev/${userID}/feed`);
+	if (res.status === 404) {
+		throw new Error(`user ${userID} not found`);
+	}
+	const xml = await res.text();
+	const feed = await new XMLParser().parse(xml) as RSSData;
+	return feed;
+}
